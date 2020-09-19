ccddf8f3f75a5d889e4d6c6fe04af87a7257b457
cychen <YeeChen0207@gmail.com>
2020-09-19 21:06:15 +0800

    feat: rewrite some functions and now behave correctly

--- sketch.js
+++ sketch.js
@@ -2,17 +2,21 @@ let WIDTH
 let HEIGHT
 let DEPTH
 
-let COLOR = [30, 255, 130]
-let DISTANCE = 50
-let ANGLE = 150
-let NUM_BOIDS = 300
+let NUM_BOIDS = 400
 
-let VECT_LIMIT = 3
+let DIST = 100
+let ANGLE = 120
+let VEL_LIMIT = 50
+let SEP_STEP = 0.1
+let ALI_STEP = 0.1
+let COH_STEP = 0.1
+let SEP_DIST = 100
+let ALI_DIST = 100
+let COH_DIST = 100
 
-let INIT_STEP = 3
-let SEP_STEP = 1
-let ALI_STEP = 1
-let COH_STEP = 1
+let BG_COLOR = [0x07, 0x00, 0x0E]
+let AXIS_COLOR = [0xF0, 0x8B, 0x33]
+let BOID_COLOR = [0xD7, 0x54, 0x04]
 
 let currBoids
 let prevBoids
@@ -20,9 +24,8 @@ let prevBoids
 function setup() {
   WIDTH = windowWidth
   HEIGHT = windowHeight
-  DEPTH = WIDTH
+  DEPTH = WIDTH + HEIGHT
   createCanvas(WIDTH, HEIGHT, WEBGL)
-  debugMode(AXES)
   angleMode(DEGREES)
 
 
@@ -43,9 +46,20 @@ function mousePressed() {
 }
 
 function draw() {
-  background(100, 200, 255)
+  background(BG_COLOR)
   ambientLight(100)
-  pointLight(255, 255, 255, 0, 0, DEPTH*2)
+  pointLight(0xFF, 0xFF, 0xFF, -DEPTH/2, -HEIGHT/2, DEPTH/2)
+
+  stroke(AXIS_COLOR)
+  strokeWeight(10)
+  line(WIDTH/2, HEIGHT/2, -DEPTH/2, WIDTH/2, HEIGHT/2, DEPTH/2)
+  line(WIDTH/2, -HEIGHT/2, -DEPTH/2, WIDTH/2, -HEIGHT/2, DEPTH/2)
+  line(-WIDTH/2, HEIGHT/2, -DEPTH/2, -WIDTH/2, HEIGHT/2, DEPTH/2)
+  line(-WIDTH/2, -HEIGHT/2, -DEPTH/2, -WIDTH/2, -HEIGHT/2, DEPTH/2)
+  line(WIDTH/2, HEIGHT/2, -DEPTH/2, -WIDTH/2, HEIGHT/2, -DEPTH/2)
+  line(WIDTH/2, HEIGHT/2, -DEPTH/2, WIDTH/2, -HEIGHT/2, -DEPTH/2)
+  line(WIDTH/2, -HEIGHT/2, -DEPTH/2, -WIDTH/2, -HEIGHT/2, -DEPTH/2)
+  line(-WIDTH/2, HEIGHT/2, -DEPTH/2, -WIDTH/2, -HEIGHT/2, -DEPTH/2)
 
   for (let boid of currBoids) {
     boid.draw()
@@ -60,101 +74,105 @@ function draw() {
 
 class Boid {
   constructor() {
-    this.coord = new p5.Vector(random(-WIDTH/2, WIDTH/2), random(-HEIGHT/2, HEIGHT/2), random(-DEPTH/2, DEPTH/2))
-    this.vect = p5.Vector.mult(p5.Vector.random3D(), INIT_STEP)
-    this.color = [random(256), random(256), random(256)]
-    this.dist = DISTANCE
-    this.angle = ANGLE
+    this.position = new p5.Vector(random(-WIDTH/2, WIDTH/2), random(-HEIGHT/2, HEIGHT/2), random(-DEPTH/2, DEPTH/2))
+    this.velocity = p5.Vector.mult(p5.Vector.random3D(), VEL_LIMIT)
+    this.color = BOID_COLOR
   }
 
-  searchNeighbors(boids) {
+  __searchNeighbors(boids) {
     let getPointsDist = function(p1, p2) { return p1.dist(p2) }
     let getVectorsAngle = function(v1, v2) { return acos(v1.dot(v2) / (v1.mag() * v2.mag())) }
 
     let neighbors = new Array()
 
     for (let boid of boids) {
-      let dist = getPointsDist(this.coord, boid.coord)
-      let angle = getVectorsAngle(this.vect, p5.Vector.sub(boid.coord, this.coord))
-      if (dist < DISTANCE && angle < ANGLE) {
+      let testDist = getPointsDist(this.position, boid.position)
+      let testAngle = getVectorsAngle(this.velocity, p5.Vector.sub(boid.position, this.position))
+      if (testDist < DIST && testAngle < ANGLE) {
         neighbors.push(boid)
       }
     }
+
     return neighbors
   }
 
-  separate(neighbors) {
-    let v = new p5.Vector(0, 0, 0)
+  __separate(neighbors) {
+    // let neighbors = this.__searchNeighbors(boids, SEP_DIST, ANGLE)
+    let velocity = new p5.Vector(0, 0, 0)
 
     for (let neighbor of neighbors) {
-      let vPrime = p5.Vector.sub(neighbor.coord, this.coord)
-      v.add(vPrime)
+      velocity.sub(p5.Vector.sub(neighbor.position, this.position))
     }
 
-    return p5.Vector.mult(v, -1)
+    return velocity
   }
 
-  align(neighbors) {
-    let v = this.vect.copy()
+  __align(neighbors) {
+    // let neighbors = this.__searchNeighbors(boids, ALI_DIST, ANGLE)
+    let velocity = this.velocity.copy()
 
     for (let neighbor of neighbors) {
-      v.add(neighbor.vect)
+      velocity.add(neighbor.velocity)
     }
+    velocity.div(neighbors.length + 1)
+    velocity.sub(this.velocity)
 
-    return p5.Vector.div(v, neighbors.length + 1)
+    return velocity
   }
 
-  cohere(neighbors) {
-    let m = new p5.Vector(0, 0, 0)
+  __cohere(neighbors) {
+    // let neighbors = this.__searchNeighbors(boids, COH_DIST, ANGLE)
+    let position = this.position.copy()
 
     for (let neighbor of neighbors) {
-      m.add(neighbor.coord)
+      position.add(neighbor.position)
     }
-    m.div(neighbors.length + 1)
+    position.div(neighbors.length + 1)
 
-    if (m.x == 0 && m.y == 0 && m.z == 0) {
-      return m
-    }
-    return p5.Vector.sub(m, this.coord)
+    let velocity = p5.Vector.sub(position, this.position)
+
+    return velocity
   }
 
   update(boids) {
-    let neighbors = this.searchNeighbors(boids)
-    let vS = this.separate(neighbors)
-    let vA = this.align(neighbors)
-    let vC = this.cohere(neighbors)
-    // this.vect.add(p5.Vector.mult(vS, SEP_STEP))
-    this.vect.add(p5.Vector.mult(vA, ALI_STEP))
-    // this.vect.add(p5.Vector.mult(vC, COH_STEP))
+    let neighbors = this.__searchNeighbors(boids)
+    let vS = this.__separate(neighbors)
+    let vA = this.__align(neighbors)
+    let vC = this.__cohere(neighbors)
+    vS.setMag(VEL_LIMIT)
+    vA.setMag(VEL_LIMIT)
+    vC.setMag(VEL_LIMIT)
+    this.velocity.add(p5.Vector.mult(vS, SEP_STEP))
+    this.velocity.add(p5.Vector.mult(vA, ALI_STEP))
+    this.velocity.add(p5.Vector.mult(vC, COH_STEP))
 
-    if (this.vect.mag() > VECT_LIMIT) {
-      this.vect.mult(VECT_LIMIT/this.vect.mag())
-    }
+    this.velocity.setMag(VEL_LIMIT)
 
-    this.coord = p5.Vector.add(this.coord, this.vect)
-    if (this.coord.x < -WIDTH/2) {
-      this.coord.x = WIDTH/2
-    } else if (this.coord.x > WIDTH/2) {
-      this.coord.x = -WIDTH/2
+    this.position = p5.Vector.add(this.position, this.velocity)
+
+    if (this.position.x < -WIDTH/2) {
+      this.position.x = WIDTH/2
+    } else if (this.position.x > WIDTH/2) {
+      this.position.x = -WIDTH/2
     }
-    if (this.coord.y < -HEIGHT/2) {
-      this.coord.y = HEIGHT/2
-    } else if (this.coord.y > HEIGHT/2) {
-      this.coord.y = -HEIGHT/2
+    if (this.position.y < -HEIGHT/2) {
+      this.position.y = HEIGHT/2
+    } else if (this.position.y > HEIGHT/2) {
+      this.position.y = -HEIGHT/2
     }
-    if (this.coord.z < -DEPTH/2) {
-      this.coord.z = DEPTH/2
-    } else if (this.coord.z > DEPTH/2) {
-      this.coord.z = -DEPTH/2
+    if (this.position.z < -DEPTH/2) {
+      this.position.z = DEPTH/2
+    } else if (this.position.z > DEPTH/2) {
+      this.position.z = -DEPTH/2
     }
   }
 
   draw() {
     push()
-    translate(this.coord.x, this.coord.y, this.coord.z)
+    translate(this.position)
     fill(this.color)
     noStroke()
-    sphere(10)
+    sphere(8)
     pop()
   }
 }
