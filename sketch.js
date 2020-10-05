89e68fd99226b33d3bb4aff0fd5701f5d658e4c8
cychen <YeeChen0207@gmail.com>
2020-10-05 19:47:00 +0800

    chore: add comment

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ let HEIGHT
 let DEPTH
 
 let NUM_BOIDS = 1000
-let VEL_LIMIT = 10
+let VEL_LIMIT = 8 // limit of velocity
 
 let DIST = 100
 let ANGLE = 120
-let SEP_STEP = 0.1
-let ALI_STEP = 0.1
-let COH_STEP = 0.1
+let SEP_MULTIPLIER = 0.1
+let ALI_MULTIPLIER = 0.1
+let COH_MULTIPLIER = 0.1
 
 let BG_COLOR = [0x07, 0x00, 0x0E]
 let AXIS_COLOR = [0xF0, 0x8B, 0x33]
@@ -19,13 +19,17 @@ let currBoids
 let prevBoids
 
 function setup() {
+  // set width, height, and depth according to the browser
   WIDTH = windowWidth
   HEIGHT = windowHeight
   DEPTH = WIDTH + HEIGHT
+
+  // initial setup for drawing
   createCanvas(WIDTH, HEIGHT, WEBGL)
   angleMode(DEGREES)
   reset()
 
+  // show frame rate every second in the console
   setInterval(function()  { console.log(frameRate()) }, 1000)
 }
 
@@ -43,10 +47,14 @@ function mousePressed() {
 }
 
 function draw() {
+  // set background color
   background(BG_COLOR)
+
+  // set light location
   ambientLight(100)
   pointLight(0xFF, 0xFF, 0xFF, -DEPTH/2, -HEIGHT/2, DEPTH/2)
 
+  // draw the cuboid that boids move around
   stroke(AXIS_COLOR)
   strokeWeight(5)
   line(WIDTH/2, HEIGHT/2, -DEPTH/2, WIDTH/2, HEIGHT/2, DEPTH/2)
@@ -58,15 +66,19 @@ function draw() {
   line(WIDTH/2, -HEIGHT/2, -DEPTH/2, -WIDTH/2, -HEIGHT/2, -DEPTH/2)
   line(-WIDTH/2, HEIGHT/2, -DEPTH/2, -WIDTH/2, -HEIGHT/2, -DEPTH/2)
 
+  // draw each boid
   for (let boid of currBoids) {
     boid.draw()
   }
 
+  // deep copy currBoids
   prevBoids = [...currBoids].map(i => ({ ...i}))
 
+  // build octree to speed up the searching of neighbors
   let octree = new Octree(new Cuboid(new p5.Vector(0, 0, 0), new p5.Vector(WIDTH/2, HEIGHT/2, DEPTH/2)), 100)
   octree.build(prevBoids)
 
+  // update the location of each boid
   for (let boid of currBoids) {
     let searchCuboid = new Cuboid(boid.position, new p5.Vector(DIST, DIST, DIST))
     boid.update(octree.search(searchCuboid))
@@ -77,11 +89,12 @@ class Boid {
   constructor() {
     this.position = new p5.Vector(random(-WIDTH/2, WIDTH/2), random(-HEIGHT/2, HEIGHT/2), random(-DEPTH/2, DEPTH/2))
     this.velocity = p5.Vector.random3D()
-    this.velocity.setMag(VEL_LIMIT)
+    this.velocity.setMag(VEL_LIMIT) // set the magnitude of velocity to VEL_LIMIT
     this.color = BOID_COLOR
   }
 
   _searchNeighbors(boids) {
+    // set some lambda functions for further usage
     let getPointsDist = function(p1, p2) { return p1.dist(p2) }
     let getVectorsAngle = function(v1, v2) { return acos(v1.dot(v2) / (v1.mag() * v2.mag())) }
 
@@ -138,17 +151,18 @@ class Boid {
     let vS = this._separate(neighbors)
     let vA = this._align(neighbors)
     let vC = this._cohere(neighbors)
-    vS.setMag(VEL_LIMIT)
-    vA.setMag(VEL_LIMIT)
-    vC.setMag(VEL_LIMIT)
-    this.velocity.add(p5.Vector.mult(vS, SEP_STEP))
-    this.velocity.add(p5.Vector.mult(vA, ALI_STEP))
-    this.velocity.add(p5.Vector.mult(vC, COH_STEP))
+    vS.setMag(VEL_LIMIT) // set the magnitude of the velocity of alignment to VEL_LIMIT
+    vA.setMag(VEL_LIMIT) // set the magnitude of the velocity of seperation to VEL_LIMIT
+    vC.setMag(VEL_LIMIT) // set the magnitude of the velocity of cohesion to VEL_LIMIT
+    this.velocity.add(p5.Vector.mult(vS, SEP_MULTIPLIER))
+    this.velocity.add(p5.Vector.mult(vA, ALI_MULTIPLIER))
+    this.velocity.add(p5.Vector.mult(vC, COH_MULTIPLIER))
 
-    this.velocity.setMag(VEL_LIMIT)
+    this.velocity.setMag(VEL_LIMIT) // set the magnitude of velocity to VEL_LIMIT
 
-    this.position = p5.Vector.add(this.position, this.velocity)
+    this.position = p5.Vector.add(this.position, this.velocity) // update the position
 
+    // handle the situation of out of boundary
     if (this.position.x < -WIDTH/2) {
       this.position.x = WIDTH/2
     } else if (this.position.x > WIDTH/2) {
@@ -176,6 +190,7 @@ class Boid {
   }
 }
 
+// the class acts as the basic component for the node of an octree
 class Cuboid {
   constructor(center, halfLength) {
     this.x = center.x
@@ -190,6 +205,7 @@ class Cuboid {
     return (o - range <= p) && (p <= o + range)
   }
 
+  // check if a point is inside this cuboid
   isContainable(point) {
     return (
       this._isInRange(point.x, this.x, this.w) &&
@@ -198,6 +214,7 @@ class Cuboid {
     )
   }
 
+  // check if this cuboid is overlapped with the other cuboid
   isOverlapped(other) {
     return (
       this._isInRange(other.x - other.w, this.x, this.w) ||
@@ -210,6 +227,7 @@ class Cuboid {
   }
 }
 
+// the class acts as the node for an octree
 class OctreeNode {
   constructor(cuboid, maxPoints) {
     this.cuboid = cuboid
@@ -293,6 +311,7 @@ class OctreeNode {
   }
 }
 
+// wrapper class of OctreeNode, provide better utility of octree
 class Octree {
   constructor(cuboid, maxPoints) {
     this.root = new OctreeNode(cuboid, maxPoints || 25)
